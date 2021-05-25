00f6f64bd15e9dbd9d60aa31d7399950928216ac
Alberto Puentes <albertopuentes00@gmail.com>
2021-05-25 16:21:25 -0500

    Acquire.py

--- acquire.py
+++ acquire.py
@@ -18,7 +18,7 @@ def telco_data():
     This function reads the telco_churn data from the Codeup db into a df
     '''
     
-    # Create SQL query.
+    # Create SQL query
     sql_query = '''SELECT * FROM customers
                 JOIN contract_types USING (contract_type_id)
                 JOIN internet_service_types USING (internet_service_type_id)
@@ -28,3 +28,23 @@ def telco_data():
     df = pd.read_sql(sql_query, get_connection('telco_churn'))
 
     return df
+
+# Cache'ing Telco Data
+def cache_telco_data(cached=False):
+    '''
+    This function reads in telco_churn data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in iris df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('telco_df.csv') == False:
+
+        # Read fresh data from db into a DataFrame
+        df = telco_data()
+
+        # Cache data
+        df.to_csv('telco_data.csv')
+
+    else:
+
+        # If csv file exists or cached == True, read in data from csv file.
+        df = pd.read_csv('telco_data.csv', index_col=0)
