39ab543b10384702bbb40c8b09050e391e99d27f
Alberto Puentes <albertopuentes00@gmail.com>
2021-05-27 13:38:23 -0500

    Prepare.py

---
+++ prepare.py
@@ -0,0 +1,28 @@
+# This function takes in the telco_df generated via acquire.telco_data() function and prepares the data to be split into train, validate & test dataframes 
+
+def prep_df(telco_df):
+    # Drop redundant data column 
+    prep_df = telco_df.drop(columns=['customer_id'])
+    
+    # convert the total_charges spaces to zeros  
+    prep_df['total_charges'] = prep_df['total_charges'].replace(' ', 0)
+    
+    # Convert total_charges from a str to a float
+    prep_df['total_charges'] = prep_df['total_charges'].astype(float)
+    
+    # Drop columns with reference data
+    prep_df = prep_df.drop(columns=['contract_type', 'internet_service_type', 'payment_type'])
+    
+    # Replace Yes/No entries with 1/0 across entire df
+    prep_df = prep_df.replace({'Yes':1, 'No':0})
+    
+    # Assumptive Manipulation: conversion of non-service to a 'No'
+    prep_df = prep_df.replace({'No internet service':0, 'No phone service':0})
+    
+    # convert the gender column to is_male and male/female strings to 1/0 values
+    prep_df = prep_df.rename(columns={'gender': 'is_male'})
+
+    # replace male/female with 1/0
+    prep_df = prep_df.replace({'Male':1, 'Female':0})
+    
+    return prep_df   
